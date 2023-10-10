2bb649fe65111056ec012e33895a6d738a572fb1
Alexey <alexey.kostenyu@gmail.com>
2023-10-10 22:52:46 +0400

    02 - Some changes in logic

--- mailer/models.py
+++ mailer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+class Boat(models.Model):
+    name = models.CharField(max_length=50, verbose_name='название')
+    year = models.PositiveSmallIntegerField(**NULLABLE, verbose_name='год выпуска')
+
+
---
+++ mailer/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from mailer.apps import MailerConfig
+
+app_name = MailerConfig.name
+
+urlpatterns = [
+    
+]
